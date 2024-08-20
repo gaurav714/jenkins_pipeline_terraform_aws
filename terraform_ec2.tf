093db0d68a257f5ea3444ee6d77b4f1f27976a5d
gaurav <gauravnautiyal77@gmail.com>
2024-08-20 10:32:31 +0530

    changes

--- terraform_ec2.tf
+++ terraform_ec2.tf
@@ -21,13 +21,3 @@ resource "aws_instance" "first_instance" {
     Name = "first-micro-instance"
   }
 }
-
-# Create the second EC2 instance
-resource "aws_instance" "second_instance" {
-  ami           = "ami-0a4408457f9a03be3" # Amazon Linux 2 AMI (replace with your desired AMI)
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "second-micro-instance"
-  }
-}
