7f9155f5fbeaf3980ed92ae5d9d12db302774d5e
gaurav <gauravnautiyal77@gmail.com>
2025-01-20 16:58:50 +0530

    outputs for ansible to use as inputs
    after creating the ec2 instance terraform will output the ec2 details such as its public and private ips along with its id

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,7 +7,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/gaurav714/jenkins_pipeline_terraform_aws.git', branch: 'main'  // Replace with your Git repository URL
+                git url: 'https://github.com/gaurav714/jenkins_pipeline_terraform_aws.git', branch: 'main'  
             }
         }
 
--- terraform_ec2.tf
+++ terraform_ec2.tf
@@ -14,10 +14,25 @@ provider "aws" {
 
 # Create the first EC2 instance
 resource "aws_instance" "first_instance" {
-  ami           = "ami-0a4408457f9a03be3" # Amazon Linux 2 AMI (replace with your desired AMI)
+  ami           = "ami-0a4408457f9a03be3" # Amazon Linux 2 AMI for the asiap-south-1 region
   instance_type = "t2.micro"
 
   tags = {
     Name = "first-micro-instance"
   }
+}
+# Output the instance details
+output "instance_public_ip" {
+  value       = aws_instance.first_instance.public_ip
+  description = "The public IP of the EC2 instance"
+}
+
+output "instance_private_ip" {
+  value       = aws_instance.first_instance.private_ip
+  description = "The private IP of the EC2 instance"
+}
+
+output "instance_id" {
+  value       = aws_instance.first_instance.id
+  description = "The ID of the EC2 instance"
 }
